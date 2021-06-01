2a9474adbf0238737b8c82220e7947e0b97d58b8
vargazsolt0924 <vargazsolt0348@gmail.com>
2021-06-01 21:56:30 +0200

    JS szerkeztése

--- html/osszehasonlitas/js/osszehasonlit.js
+++ html/osszehasonlitas/js/osszehasonlit.js
@@ -1,9 +1,109 @@
-window.addEventListener("load", init, false);
-
 function $(id){
     return document.getElementById(id);
+}
+
+var tipus1 = {
+    mercedes: true,
+    nev: "C220d limousine",
+    motor: "2.0 TDI",
+    teljesitmeny: "194Le(143kw)",
+    gyorsulas: "0-100: 7,7s",
+    vegsebesseg: "228 km/h",
+    fogyasztas: "6,2 l/100km",
+}
+
+var tipus2 = {
+    mercedes: true,
+    nev: "E300de",
+    motor: "2.0 TDI",
+    teljesitmeny: "194Le(143kw)",
+    gyorsulas: "0-100: 5,9s",
+    vegsebesseg: "250 km/h",
+    fogyasztas: "8,5 l/100km",
+}
+
+var tipus3 = {
+    mercedes: true,
+    nev: "GLE400d",
+    motor: "3.0 TDI",
+    teljesitmeny: "333Le(245kw)",
+    gyorsulas: "0-100: 5,7s",
+    vegsebesseg: "245 km/h",
+    fogyasztas: "8,7 l/100km",
+}
+
+var tipus4 = {
+    mercedes: true,
+    nev: "S450",
+    motor: "5.0 (V8)",
+    teljesitmeny: "340Le(250kw)",
+    gyorsulas: "0-100: 5,9s",
+    vegsebesseg: "250 km/h",
+    fogyasztas: "11,1 l/100km",
+}
+
+var tipus5 = {
+    mercedes: true,
+    nev: "GT63",
+    motor: "4.0 (V8)",
+    teljesitmeny: "639Le(470kw)",
+    gyorsulas: "0-100: 3,2s",
+    vegsebesseg: "315 km/h",
+    fogyasztas: "11,3 l/100km",
+}
+
+var tipus6 = {
+    mercedes: false,
+    nev: "A3",
+    motor: "1.5 TFSI",
+    teljesitmeny: "150Le(110kw)",
+    gyorsulas: "0-100: 8,2s",
+    vegsebesseg: "224 km/h",
+    fogyasztas: "5,5 l/100km",
+}
+
+var tipus7 = {
+    mercedes: false,
+    nev: "A6",
+    motor: "3.0 TDI",
+    teljesitmeny: "340Le(250kw)",
+    gyorsulas: "0-100: 5,5s",
+    vegsebesseg: "250 km/h",
+    fogyasztas: "7,6 l/100km",
+}
+
+var tipus8 = {
+    mercedes: false,
+    nev: "A8L",
+    motor: "3.0 TDI",
+    teljesitmeny: "286Le(210kw)",
+    gyorsulas: "0-100: 5,9s",
+    vegsebesseg: "250 km/h",
+    fogyasztas: "5,8 l/100km",
+}
+
+var tipus9 = {
+    mercedes: false,
+    nev: "Q7",
+    motor: "3.0 TDI",
+    teljesitmeny: "286Le(210kw)",
+    gyorsulas: "0-100: 6,3s",
+    vegsebesseg: "241 km/h",
+    fogyasztas: "6,5 l/100km",
+}
+
+var tipus10 = {
+    mercedes: false,
+    nev: "R8",
+    motor: "5.2 TFSI",
+    teljesitmeny: "610Le(455kw)",
+    gyorsulas: "0-100: 3,2s",
+    vegsebesseg: "330 km/h",
+    fogyasztas: "12,3 l/100km",
 }
 
 function init(){
 
 }
+
+window.addEventListener("load", init, false);
